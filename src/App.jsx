cbb6ea80e899e3a55e8cd3cd808e47bc540dc3bd
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2024-08-22 17:46:01 -0700

    Added background blur, fade in effect

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Header from './components/Header/Header';
 import Projects from './components/Projects/Projects.jsx';
 import SecondaryNavbar from './components/SecondaryNavbar/SecondaryNavbar';
 
-import {useState} from 'react';
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import { useState } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import './App.css';
 
@@ -16,31 +16,63 @@ import './App.css';
 function App() {
 
   const [sectionToDisplay, setSectionToDisplay] = useState('');
+  const [backgroundActive, setBackgroundActive] = useState(false);
 
   const handleChangeDisplay = (section) => {
-    setSectionToDisplay(section);
+    if (section !== sectionToDisplay) {
+      setBackgroundActive(true);
+      setTimeout(() => {
+        setSectionToDisplay(section);
+      }, 300);
+
+      setTimeout(() => {
+        setBackgroundActive(false);
+      }, 525); 
+    }
   }
 
   return (
     <>
       <Header />
-      <SecondaryNavbar sectionChange = {handleChangeDisplay} />
-      <TransitionGroup>
-        <CSSTransition
-          key={sectionToDisplay}
-          timeout={500}
-          classNames="fade"
-        >
-          <div>
-          {sectionToDisplay === 'projects' && <Projects />}
-            {sectionToDisplay === 'certifications' && <Certifications />}
-            {sectionToDisplay === 'about' && <About /> || sectionToDisplay === '' && <About />}
-          </div>
-        </CSSTransition>
+      <SecondaryNavbar sectionChange={handleChangeDisplay} />
+      <div className={`background-blur ${backgroundActive ? 'background-blur-active' : ''}`} />
+      <TransitionGroup component={null}>
+        {sectionToDisplay === 'projects' && (
+          <CSSTransition
+            key="projects"
+            timeout={300}
+            classNames="fade"
+          >
+            <div>
+              <Projects />
+            </div>
+          </CSSTransition>
+        )}
+        {sectionToDisplay === 'certifications' && (
+          <CSSTransition
+            key="certifications"
+            timeout={300}
+            classNames="fade"
+          >
+            <div>
+              <Certifications />
+            </div>
+          </CSSTransition>
+        )}
+        {(sectionToDisplay === 'about' || sectionToDisplay === '') && (
+          <CSSTransition
+            key="about"
+            timeout={300}
+            classNames="fade"
+          >
+            <div>
+              <About />
+            </div>
+          </CSSTransition>
+        )}
       </TransitionGroup>
     </>
-  )
-
+  );
 }
 
 export default App
