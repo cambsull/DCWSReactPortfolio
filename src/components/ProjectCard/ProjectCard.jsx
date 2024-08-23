986cbff6a122bef8f0a1e16cbfc1a7bd49c85267
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2024-08-22 17:11:40 -0700

    Various mobile improvements

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -7,15 +7,10 @@ function ProjectCard({ title, description, image, link }) {
 
     return (
         <div className="projectCard">
-            <img src={image} alt={title} className="projectCardImage" />
+            <img src={image} alt={title} onClick={() => window.open(link, '_blank')} className="projectCardImage" />
             <div className="projectCardContent">
                 <h3>{title}</h3>
                 <p>{description}</p>
-
-                <button className="projectButton" onClick={() => window.open(link, '_blank')}>
-                    View Project
-                </button>
-        
             </div>
         </div>
     );
