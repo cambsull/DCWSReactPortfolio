38cb2f132b627e8b4f8548f5670abffaf5284fa2
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2024-08-31 22:13:43 -0700

    Update icon

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -57,7 +57,7 @@ function About({onChangeSection}) {
                             <p>Python</p>
                         </div>
                         <div className="techEntry">
-                            <i class="fa-brands fa-microsoft" title=".NET/C#"></i>
+                            <i class="fas fa-code" title="C#"></i>
                             <p>.NET/C#</p>
                         </div>
                         <div className="techEntry">
